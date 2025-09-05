30d070fc9d7289326c49a0ea378c2931c974f7f8
minhi0449 <minhi0449@gmail.com>
2025-09-05 21:16:41 +0900

    signed 와 unsigned 의 차이

---
+++ 1-2장/2-8/2-8.c
@@ -0,0 +1,14 @@
+// signed 와 unsigned 의 차이
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned short a = 32766;
+    unsigned short b = 10;
+    unsigned short c = a + b;
+    int d = a + b;
+
+    printf("정수형 a의 값 %d와 b의 값 %d의 합은 %d이다. \n", a, b, c);
+    printf("정수형 a의 값 %d와 b의 값 %d의 합은 %d이다. \n", a, b, d);
+    return 0;
+}
