ce08e7ac99e819aaa7cd62e50cb94df19eda35b2
minhi0449 <minhi0449@gmail.com>
2025-09-07 20:20:17 +0900

    산술 연산자

---
+++ 3장/3-1/3-1.c
@@ -0,0 +1,15 @@
+// 산술 연산자
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 5;
+    int b = 3;
+
+    printf("두 수의 합은 %d 입니다. \n", a + b);
+    printf("두 수의 차은 %d 입니다. \n", a - b);
+    printf("두 수의 곱은 %d 입니다. \n", a * b);
+    printf("두 수의 나누기는 %d 입니다. \n", a / b);
+    printf("두 수의 나머지는 %d 입니다. \n", a % b);
+    return 0;
+}
