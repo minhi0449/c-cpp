0a410158348989fa9dabe0825e0dbb90c705832f
minhi0449 <minhi0449@gmail.com>
2025-09-07 20:40:52 +0900

    대입 연산자

---
+++ 3장/3-2/3-2.c
@@ -0,0 +1,20 @@
+// 대입 연산자
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 5;
+    int b = 3;
+
+    a += b;
+    printf("a += b의 결과는 %d 입니다. \n", a);
+    a -= b;
+    printf("a -= b의 결과는 %d 입니다. \n", a);
+    a *= b;
+    printf("a *= b의 결과는 %d 입니다. \n", a);
+    a /= b;
+    printf("a /= b의 결과는 %d 입니다. \n", a);
+    a %= b;
+    printf("a %= b의 결과는 %d 입니다. \n", a);
+    return 0;
+}
